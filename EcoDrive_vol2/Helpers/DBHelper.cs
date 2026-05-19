d2a3b3558d33a28528df04da2ec9431e91a58b17
arumiprstyn <langgengarum41@gmail.com>
2026-05-19 22:12:03 +0700

    add dbhelper

---
+++ EcoDrive_vol2/Helpers/DBHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace EcoDrive_vol2.Helpers
+{
+    class DatabaseHelper
+    {
+        private static string connString = "Host=localhost;Port=5432;Database=KoneksiDB;Username=postgres;Password=langgeng847";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
