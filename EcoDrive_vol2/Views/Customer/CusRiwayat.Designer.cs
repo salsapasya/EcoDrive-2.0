1bc6537a52af2233f8b6406356cae40d868918bb
rachelsyaf <rachelfirasyaa03@gmail.com>
2026-05-27 10:45:08 +0000

    lihat riwayat transaksi

--- EcoDrive_vol2/Views/Customer/CusRiwayat.Designer.cs
+++ EcoDrive_vol2/Views/Customer/CusRiwayat.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CusRiwayat";
+            dgvRiwayat = new DataGridView();
+            lblTitle = new Label();
+            btnKembali = new Button();
+            colId = new DataGridViewTextBoxColumn();
+            colKendaraan = new DataGridViewTextBoxColumn();
+            colTanggal = new DataGridViewTextBoxColumn();
+            colDurasi = new DataGridViewTextBoxColumn();
+            colTotal = new DataGridViewTextBoxColumn();
+            colStatus = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvRiwayat).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvRiwayat
+            // 
+            dgvRiwayat.AllowUserToAddRows = false;
+            dgvRiwayat.AllowUserToOrderColumns = true;
+            dgvRiwayat.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvRiwayat.BorderStyle = BorderStyle.Fixed3D;
+            dgvRiwayat.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dgvRiwayat.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRiwayat.Columns.AddRange(new DataGridViewColumn[] { colId, colKendaraan, colTanggal, colDurasi, colTotal, colStatus });
+            dgvRiwayat.EnableHeadersVisualStyles = false;
+            dgvRiwayat.GridColor = Color.Black;
+            dgvRiwayat.Location = new Point(31, 77);
+            dgvRiwayat.Name = "dgvRiwayat";
+            dgvRiwayat.ReadOnly = true;
+            dgvRiwayat.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dgvRiwayat.RowHeadersVisible = false;
+            dgvRiwayat.Size = new Size(732, 318);
+            dgvRiwayat.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Elephant", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.DarkOliveGreen;
+            lblTitle.Location = new Point(31, 30);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(226, 27);
+            lblTitle.TabIndex = 1;
+            lblTitle.Text = "Riwayat Transaksi";
+            // 
+            // btnKembali
+            // 
+            btnKembali.Location = new Point(713, 415);
+            btnKembali.Name = "btnKembali";
+            btnKembali.Size = new Size(75, 23);
+            btnKembali.TabIndex = 2;
+            btnKembali.Text = "Kembali";
+            btnKembali.UseVisualStyleBackColor = true;
+            // 
+            // colId
+            // 
+            colId.HeaderText = "ID Transaksi";
+            colId.Name = "colId";
+            colId.ReadOnly = true;
+            // 
+            // colKendaraan
+            // 
+            colKendaraan.HeaderText = "Kendaraan";
+            colKendaraan.Name = "colKendaraan";
+            colKendaraan.ReadOnly = true;
+            // 
+            // colTanggal
+            // 
+            colTanggal.HeaderText = "Tanggal";
+            colTanggal.Name = "colTanggal";
+            colTanggal.ReadOnly = true;
+            // 
+            // colDurasi
+            // 
+            colDurasi.HeaderText = "Durasi";
+            colDurasi.Name = "colDurasi";
+            colDurasi.ReadOnly = true;
+            // 
+            // colTotal
+            // 
+            colTotal.HeaderText = "Total";
+            colTotal.Name = "colTotal";
+            colTotal.ReadOnly = true;
+            // 
+            // colStatus
+            // 
+            colStatus.HeaderText = "Status";
+            colStatus.Name = "colStatus";
+            colStatus.ReadOnly = true;
+            // 
+            // CusRiwayat
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnKembali);
+            Controls.Add(lblTitle);
+            Controls.Add(dgvRiwayat);
+            Name = "CusRiwayat";
+            Text = "CusRiwayat";
+            Load += CusRiwayat_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvRiwayat).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgvRiwayat;
+        private Label lblTitle;
+        private DataGridViewTextBoxColumn colId;
+        private DataGridViewTextBoxColumn colKendaraan;
+        private DataGridViewTextBoxColumn colTanggal;
+        private DataGridViewTextBoxColumn colDurasi;
+        private DataGridViewTextBoxColumn colTotal;
+        private DataGridViewTextBoxColumn colStatus;
+        private Button btnKembali;
     }
 }
--- EcoDrive_vol2/Views/Customer/CusRiwayat.cs
+++ EcoDrive_vol2/Views/Customer/CusRiwayat.cs
@@ -17,5 +17,69 @@ namespace EcoDrive_vol2.Views
             InitializeComponent();
             this.BackColor = bgUtama;
         }
+
+        private void CusRiwayat_Load(object sender, EventArgs e)
+        {
+            // Tampilan DataGridView
+            dgvRiwayat.AutoSizeColumnsMode =
+                DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgvRiwayat.AllowUserToAddRows =
+                false;
+
+            dgvRiwayat.RowHeadersVisible =
+                false;
+
+            dgvRiwayat.ReadOnly =
+                true;
+
+            dgvRiwayat.CellBorderStyle =
+                DataGridViewCellBorderStyle.Single;
+
+            dgvRiwayat.GridColor =
+                Color.Black;
+
+            // Dummy Data Riwayat
+            dgvRiwayat.Rows.Add(
+                "TRX001",
+                "Tesla Model 3",
+                "27 Mei 2026",
+                "2 Hari",
+                "Rp 800.000",
+                "Selesai");
+
+            dgvRiwayat.Rows.Add(
+                "TRX002",
+                "Ioniq 5",
+                "20 Mei 2026",
+                "1 Hari",
+                "Rp 400.000",
+                "Selesai");
+
+            dgvRiwayat.Rows.Add(
+                "TRX003",
+                "Motor Gesits",
+                "15 Mei 2026",
+                "3 Hari",
+                "Rp 300.000",
+                "Selesai");
+        }
+
+        private void dgvRiwayat_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnKembali_Click(
+            object sender,
+            EventArgs e)
+        {
+            CusDasboard dashboard =
+                new CusDasboard();
+
+            dashboard.Show();
+
+            this.Hide();
+        }
     }
 }
