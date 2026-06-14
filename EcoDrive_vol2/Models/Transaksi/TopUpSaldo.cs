98d077d75bbfb2d71b4a43f3ad445b2284fcd9f7
rachelsyaf <rachelfirasyaa03@gmail.com>
2026-06-14 23:35:55 +0000

    revisi recel

--- EcoDrive_vol2/Models/Transaksi/TopUpSaldo.cs
+++ EcoDrive_vol2/Models/Transaksi/TopUpSaldo.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using EcoDrive_vol2.Models.Enums;
 
-namespace EcoDrive_vol2.Models.Transaksi 
+namespace EcoDrive_vol2.Models.Transaksi
 {
     public class TopupSaldo
     {
-        public int IdTopupSaldo { get; set; }
-
+        public TopupSaldo(int idCustomer, int jumlahTopup)
+        {
+            IdCustomer = idCustomer;
+            JumlahTopup = jumlahTopup;
+            IdTopupsaldo = 0; 
+            StatusTopup = TopupStatus.pending;
+        }
+        public int IdTopupsaldo { get; set; }
         public int IdCustomer { get; set; }
-
         public int JumlahTopup { get; set; }
-
         public TopupStatus StatusTopup { get; set; }
     }
 }
